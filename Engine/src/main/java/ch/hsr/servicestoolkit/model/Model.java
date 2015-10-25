8d90a59c955334963ad8f2842bd79e90eff17c6e
Michi Gysel <mgysel@hsr.ch>
2015-10-25 16:32:31 +0100

    refactoring

--- Engine/src/main/java/ch/hsr/servicestoolkit/model/Model.java
+++ Engine/src/main/java/ch/hsr/servicestoolkit/model/Model.java
@@ -11,6 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.google.common.base.Objects;
+
 @Entity
 public class Model {
 
@@ -45,32 +47,17 @@ public class Model {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hashCode(id);
 	}
 
 	@Override
 	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
+		if (obj instanceof Model) {
+			Model other = (Model) obj;
+			return this == other || Objects.equal(id, other.id);
+		} else {
 			return false;
 		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Model other = (Model) obj;
-		if (id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!id.equals(other.id)) {
-			return false;
-		}
-		return true;
 	}
 
 }
